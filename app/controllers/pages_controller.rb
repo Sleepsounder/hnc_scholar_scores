d7486f987277ae536576289c07de8b5fee4ee6d9
Geoff <geoff.register@gmail.com>
2020-04-12 17:27:00 -0400

    WIP find eligible applicants for user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-	def index; end
+	def index
+		@scores = Score.where(
+			user_id: current_user.id
+		)
+		@applicant = eligible_applicant
+	end
+
+	def new
+		# add change applicant available to new button in index?
+	end
+
+
+	private
+
+	# def eligible_applicant
+	# 	all_elligible_applicants = Applicant.all.select do |applicant|
+	# 		applicant.available == true && not_reviewed_by_current_user(applicant)
+	# 	end
+	# 	binding.pry
+	# end 
+
+	# def not_reviewed_by_current_user(applicant)
+	# 	user_reviewed = applicant.users.any? { |user| user.id == current_user.id }
+	# end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root to: "pages#index"
   devise_for :users
+  resources :pages
 
   ActiveAdmin.routes(self)
 
