dddaa1129ab921369bb021e5d669e565ea4ee1b3
Geoff <geoff.register@gmail.com>
2020-04-12 21:51:36 -0400

    WIP user flow in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,17 +5,18 @@ class PagesController < ApplicationController
 		@scores = Score.where(
 			user_id: current_user.id
 		)
-		@applicant = eligible_applicant
 	end
 
 	def new
 		@applicant = eligible_applicant
+		# to make unavailable while being graded!
+		# @applicant.update(available: false)
 	end
-
 
 	private
 
 	def eligible_applicant
+		# TODO: make below code less n + 1 ish  
 		available_applicants = Applicant.where(available: true)
 		eligible_selection = available_applicants.reject do |applicant|
 			applicant.users.any? { |user| user.id == current_user.id }
@@ -23,6 +24,3 @@ class PagesController < ApplicationController
 		eligible_selection.sort_by{|applicant| applicant.reviews}.first
 	end
 end
-
-
-# Category.joins(articles: [{ comments: :guest }, :tags])
