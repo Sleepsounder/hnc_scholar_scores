9a956cb357e9d9489a2ffeba7ba202f62ba7304e
Geoff <geoff.register@gmail.com>
2020-04-15 17:26:22 -0400

    lint syntax enabled false

--- spec/requests/scores_request_spec.rb
+++ spec/requests/scores_request_spec.rb
@@ -2,11 +2,3 @@
 
 require "rails_helper"
 
-# RSpec.describe "Scores", type: :request do
-#   describe "GET /create" do
-#     it "returns http success" do
-#       get "/scores/create"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
