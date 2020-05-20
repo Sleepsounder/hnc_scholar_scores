1046fd41b6b1ed84409573c1d2bcf71e47164245
Geoff <geoff.register@gmail.com>
2020-05-19 23:19:48 -0400

    csv formatted

--- app/admin/applicants.rb
+++ app/admin/applicants.rb
@@ -9,7 +9,6 @@ ActiveAdmin.register Applicant do
     id_column
     column :first_name
     column :last_name
-    column :address
     column "Reviews", :review_count
     column "McCoy Count", :mccoy_count
     column :financial_avg
@@ -20,16 +19,32 @@ ActiveAdmin.register Applicant do
     actions
   end
 
-  csv force_quotes: true, col_sep: ';' do
-    column :first_name
+  csv force_quotes: true, col_sep: ";" do
+    column :id
     column :last_name
+    column :first_name
+    column :dq
+    column :reader_1
+    column :r1_mc
+    column :r1_fin
+    column :r1_ac
+    column :r1_rec
+    column :r1_ess
+    column :reader_2
+    column :r2_mc
+    column :r2_fin
+    column :r2_ac
+    column :r2_rec
+    column :r2_ess
+    column :reader_3
+    column :r3_mc
+    column :r3_fin
+    column :r3_ac
+    column :r3_rec
+    column :r3_ess
     column :financial_avg
     column :academic_avg
     column :recommend_avg
     column :essay_avg
-    column :disqualified
-    column "Reader 1" do |applicant|
-      @reader = applicant.users.first.first_name
-    end
   end
 end
--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -62,4 +62,129 @@ class Applicant < ApplicationRecord
     total = scores.inject(0) { |sum, score| sum + score.essay }
     (total / scores.count).round(2)
   end
+
+  def dq
+    if disqualified == true
+      "yes"
+    else
+      "no"
+    end
+  end
+
+  def reader_1
+    return if users.first.nil?
+
+    @reader1 = users.first
+    @reader1_score = scores&.select { |score| score.user_id == @reader1.id }
+
+    @reader1.initials
+  end
+
+  def r1_mc
+    return if @reader1_score.nil?
+
+    @reader1_score.first.mccoy_qualified
+  end
+
+  def r1_fin
+    return if @reader1_score.nil?
+
+    @reader1_score.first.financial
+  end
+
+  def r1_ac
+    return if @reader1_score.nil?
+
+    @reader1_score.first.academic
+  end
+
+  def r1_rec
+    return if @reader1_score.nil?
+
+    @reader1_score.first.recommend
+  end
+
+  def r1_ess
+    return if @reader1_score.nil?
+
+    @reader1_score.first.essay
+  end
+
+  def reader_2
+    return if users.second.nil?
+
+    @reader2 = users.second
+    @reader2_score = scores&.select { |score| score.user_id == @reader2.id }
+
+    @reader2.initials
+  end
+
+  def r2_mc
+    return if @reader2_score.nil?
+
+    @reader2_score.first.mccoy_qualified
+  end
+
+  def r2_fin
+    return if @reader2_score.nil?
+
+    @reader2_score.first.financial
+  end
+
+  def r2_ac
+    return if @reader2_score.nil?
+
+    @reader2_score.first.academic
+  end
+
+  def r2_rec
+    return if @reader2_score.nil?
+
+    @reader2_score.first.recommend
+  end
+
+  def r2_ess
+    return if @reader2_score.nil?
+
+    @reader2_score.first.essay
+  end
+
+  def reader_3
+    return if users.third.nil?
+
+    @reader3 = users.third
+    @reader3_score = scores&.select { |score| score.user_id == @reader3.id }
+
+    @reader3.initials
+  end
+
+  def r3_mc
+    return if @reader3_score.nil?
+
+    @reader3_score.first.mccoy_qualified
+  end
+
+  def r3_fin
+    return if @reader3_score.nil?
+
+    @reader3_score.first.financial
+  end
+
+  def r3_ac
+    return if @reader3_score.nil?
+
+    @reader3_score.first.academic
+  end
+
+  def r3_rec
+    return if @reader3_score.nil?
+
+    @reader3_score.first.recommend
+  end
+
+  def r3_ess
+    return if @reader3_score.nil?
+
+    @reader3_score.first.essay
+  end
 end
