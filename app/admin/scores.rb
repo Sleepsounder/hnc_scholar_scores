338c79b49ddb56868dc749d1284cc9123f60f829
Geoff <geoff.register@gmail.com>
2020-05-21 00:45:07 -0400

    active admin tweaks and rubocop config

--- app/admin/scores.rb
+++ app/admin/scores.rb
@@ -2,19 +2,40 @@
 
 ActiveAdmin.register Score do
   actions :all, except: [:destroy]
+  filter :user_last_name, as: :string, label: "Reader Last Name"
+  filter :applicant_last_name, as: :string, label: "Applicant Last Name"
+
   index do
     selectable_column
     id_column
     column "Applicant", :applicant_full_name
     column "Reader", :user_full_name
-    column :mccoy_qualified
-    column :financial
-    column :academic
-    column :recommend
-    column :essay
-    column :career
-    column :bd
-    column :comments
+    column "McCoy", :mccoy_qualified
+    column "Fin", :financial
+    column "Ac", :academic
+    column "Rec", :recommend
+    column "Imp", :essay
+    column :career do |score|
+      if score.career.length >= 10
+        "#{score.career.slice(0, 10)}..."
+      else
+        score.career
+      end
+    end
+    column "BD", :bd do |score|
+      if score.bd.length >= 10
+        "#{score.bd.slice(0, 10)}..."
+      else
+        score.bd
+      end
+    end
+    column :comments do |score|
+      if score.comments.length >= 10
+        "#{score.comments.slice(0, 10)}..."
+      else
+        score.comments
+      end
+    end
     actions
   end
 end
