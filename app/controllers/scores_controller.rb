0ae5c96c280d11343692153d2413129417de113f
Geoff <geoff.register@gmail.com>
2020-06-03 19:30:14 -0400

    add parantheses to elligible_applicants function

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -96,11 +96,9 @@ class ScoresController < ApplicationController
   def eligible_applicants
     Applicant.includes([:scores], [:users], [:removed_applicants]).select do |applicant|
       !applicant.disqualified? &&
-        applicant.scores.count + applicant.pending_scores.count < 3 &&
-        applicant.users.all? { |user| user.id != current_user.id } &&
-        applicant.removed_applicants.all? do |removed_applicant|
-          removed_applicant.user_id != current_user.id
-        end
+        (applicant.scores.count + applicant.pending_scores.count < 3) &&
+        (applicant.users.all? { |user| user.id != current_user.id }) &&
+        (applicant.removed_applicants.all? { |ra| ra.user_id != current_user.id })
     end
   end
 
